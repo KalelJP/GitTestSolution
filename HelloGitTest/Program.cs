dd97aa180fac5e820b2568a78969f78f987958e6
Kalel <kalel.jong_poeta@i-azo.ch>
2022-02-01 13:20:15 +0100

    Updated program.cs

--- HelloGitTest/Program.cs
+++ HelloGitTest/Program.cs
@@ -6,9 +6,29 @@ namespace HelloGitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Bye World!");
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Hello GitHub!");
+                Console.WriteLine("=================");
+                Console.WriteLine("Let's test your Git knowledge!");
+                Console.WriteLine();
+                Console.WriteLine("With which command is utilised to put two branches together?");
+                string sAnswer1 = Console.ReadLine();
+                if (sAnswer1.ToLower() == "merge")
+                {
+                    Console.WriteLine("Congratulations! Way to go superstar!");
+                    Console.WriteLine("Goodbye");
+                    repeat = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unfortunately that is not right. Try Again!");
+                }
+            } while (repeat);
+
+
         }
     }
 }
