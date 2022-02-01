84282a3d01c79f87619a2bdb81b716f877f8be76
Kalel <kalel.jong_poeta@i-azo.ch>
2022-02-01 11:28:06 +0100

    New console output

--- HelloGitTest/Program.cs
+++ HelloGitTest/Program.cs
@@ -8,6 +8,7 @@ namespace HelloGitTest
         {
             Console.WriteLine("Hello GitHub!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
         }
     }
 }
